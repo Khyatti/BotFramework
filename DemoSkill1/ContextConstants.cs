9d160d0e479e23a3ccdf175689d93bd73177b4ad
Khyatti <khyattigupta@gmail.com>
2017-09-27 14:32:55 +0530

    StateManagement and SuggestedActions
    DemoSkill: State Management and Suggested Actions using c# Bot framework

---
+++ DemoSkill1/ContextConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Bot.Sample.SimpleEchoBot
+{
+    public class ContextConstants
+    {
+        public const string UserName = "UserName";
+
+        public const int QuestionsAsked = 0;
+
+        public const int Score = 0;
+    }
+}
